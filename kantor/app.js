705d88d6b7ca50537ec67a866299941fa902551b
Adam Sawoscianik <emailedzika@mail.com>
2017-08-16 09:54:03 +0200

    kantorek

---
+++ kantor/app.js
@@ -0,0 +1,15 @@
+
+
+function przelicz() {
+    var zlotowki = document.getElementById('siano').value;
+    euro = zlotowki / 3.65;
+    alert(zlotowki + 'zl to jest: ' + euro + 'euro');
+    
+    
+    
+}
+
+var btn = document.getElementById('count');
+btn.addEventListener('click', function() {
+    przelicz();
+});
