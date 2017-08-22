f3f21a7b0e39a735ba94441633946f4da384945d
Adam Sawoscianik <emailedzika@mail.com>
2017-08-22 15:40:57 +0200

    aktualizacja angularHTTP

--- angularHTTP/js/app.js
+++ angularHTTP/js/app.js
@@ -22,11 +22,14 @@ app.config(function($routeProvider){
     .when('/animals',{
         templateUrl: './views/animals.html',
         controller: 'third'
+    })
+    .when('/edit',{
+        templateUrl: '/views/edit.html',
+        controller: 'first'
     });
-   
 });
 
-app.controller('first', function($scope, $http) {
+app.controller('first', function($scope, $http, $filter) {
     // $http(/*obiekt konfiguracyjny*/)
     //    .then(/*success handler*/, 
     //          /*error handler*/);
@@ -39,7 +42,58 @@ app.controller('first', function($scope, $http) {
     }, function(error) {
         console.error(error);
     });
-});
+    
+    $scope.add = function() {
+        $http({
+            url: 'http://localhost:3000/users',
+            method: "POST",
+            data: {
+                name: $scope.name,
+                lastname: $scope.lastname
+            }
+        }).then(function(success){
+            $scope.users.push(success.data);
+            $scope.name = '';
+            $scope.lastname = '';
+        }, function(error) {
+            console.error(error);
+        });
+    }
+    
+    $scope.delete = function(id) {
+        $http({
+            url: 'http://localhost:3000/users/' + id,
+            method: "DELETE",
+        }).then(function(success){
+           var deletedUser = $filter('filter')($scope.users, {id : id})[0];
+           $scope.users.splice($scope.users.indexOf(deletedUser), 1);
+        },function(error) {
+            console.error(error);
+        
+        });
+    }   
+    
+    
+    $scope.edit = function(id){
+        if($scope.editId == id){
+            $http({
+                url: 'http://localhost:3000/users/' + id,
+                method: 'PUT',
+                data: {
+                    name: $scope.name,
+                    lastname: $scope.lastname
+                }
+                
+            }).then(function(s){
+                console.log(s)
+            },function(error){
+                console.error(error);
+            }
+            });
+        }
+
+  
+
 app.controller('second', function($scope, $http, $routeParams){
     $http({ 
         url:"http://localhost:3000/users/"+ $routeParams.id,
