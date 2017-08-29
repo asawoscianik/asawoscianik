e6173c05152e033621becda3b18f1f6ed916fa79
Adam Sawoscianik <emailedzika@mail.com>
2017-08-29 15:41:18 +0200

    Strona z psdka

---
+++ PhpProject1/index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        
+        <title>PSDK</title>
+        
+        <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i&amp;subset=latin-ext" rel="stylesheet">
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+      
+        <link rel="stylesheet" href="css/main.css">
+        
+        <script  src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+        
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        
+        <script src="js/app.js"></script>
+            
+    </head>
+    <body>
+        <div id="section1">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 img">
+                        <img  src="img/logo.png"/>
+                    </div>
+                    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                        <ul>
+                            <li><a href="#section2">LIFESTYLE</a></li>
+                            <li><a href="#section4">PHOTODIARY</a></li>
+                            <li><a href="#section5">MUSIC</a></li>
+                            <li><a href="#section6">TRAVEL</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div> 
+        <div id="section2">
+            <div class="container-fuid">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <img src="img/image.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="section3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <h5>PHOTODIARY</h5>
+                        <h1>The perfect weekend getaway</h1>
+                        <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
+                        <div class="text-center">
+                            <h5>LEAVE A COMMENT</h5>
+                            <input type="text"/>
+                            <button type="button">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="section4">      
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 col-xs-12 col-md-6 col-lg-6 center-xs center-md center-lg middle-xs middle-sm middle-md middle-lg">
+                        <img class="img" src="img/image1.png">
+                        <h5>LIFESTYLE</h5>
+                        <h1>More than just amusic festival</h1>
+                        <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
+                    </div>
+                    <div class=" img col-sm-12 col-xs-12 col-md-6 col-lg-6 center-xs center-md center-lg middle-xs middle-sm middle-md middle-lg">
+                        <img src="img/image2.png">
+                        <h5>LIFESTYLE</h5>
+                        <h1>Life tastes better with coffe</h1>
+                        <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
+                    </div>
+                </div> 
+            </div>
+        </div>
+        <div id="section5">      
+            <div class="container">
+                <div class="row align-center">
+                    <div class="col-sm-12 col-xs-12 col-md-6 col-lg-6 center-xs center-md center-lg middle-xs middle-sm middle-md middle-lg">
+                        <img id="img" src="img/image3.png">
+                        <h5>PHOTOGRAPHY</h5>
+                        <h1>American dream</h1>
+                        <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
+                    </div>
+                    <div class="img col-sm-12 col-xs-12 col-md-6 col-lg-6 center-xs center-md center-lg middle-xs middle-sm middle-md middle-lg">
+                        <img src="img/image4.png">
+                        <h5>PHOTOGRAPHY</h5>
+                        <h1>A day exploring the alps</h1>
+                        <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
+                    </div>
+                </div> 
+            </div>
+        </div>
+        <div id="section6">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <div>
+                            <h2>Sing up for out newsletter!</h2>
+                            <input placeholder="email" type="text">
+                            <button type="button">Zatwierdz</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="section7">
+            <div class="container">
+                <div class="row">
+                    <div class="img col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <img src="img/image5.png">
+                        <h5>LIFESTYLE</h5>
+                        <h1>Top 10 songs for running</h1>
+                        <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ips
+                    </div>
+                    <div class="img col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <img src="img/image6.png">
+                        <h5>LIFESTYLE</h5>
+                        <h1>Cold windet days</h1>
+                        <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ips
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="section8">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <div class="text-center">
+                        <button type="button">Load more</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="section9">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                        <p>Terms and conditions Privacy</p>
+                    </div>
+                    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                        <ul>
+                            
+                         Follow   <li class="glyphicon glyphicon-search" aria-hidden="true"></li>
+                            <li class="glyphicon glyphicon-link" aria-hidden="true"></li>
+                            <li class="glyphicon glyphicon-phone" aria-hidden="true"></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+            
+        <?php
+        
+        ?>
+    </body>
+</html>
---
+++ PhpProject1/js/app.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function(){
+   var logo = document.querySelector('img');
+   console.log(logo);
+});
+
+
+console.log(a);
+
+
